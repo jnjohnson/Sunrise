36bf3259df157f3302fc724b361bdb34c2a29cc8
jimmy johnson <jimmy@jnjohnson.io>
2018-06-22 14:03:22 -0500

    rewrote component so starField shifts based on scroll direction, to give the effect of a camera panning. Also made instances of star component increase and decrease opacity, to give the effect of stars twinkling

--- src/StarField.js
+++ src/StarField.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const shiftUpEnd    = 3000;
+const shiftUpEnd = 3000;
+const fadeAnimationLength = 3000;
+const fadeAnimationFrames = 60;
 
 class StarField extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            top: 0
+        }
+
+        this.shiftStars = this.shiftStars.bind(this);
+    }
+
     componentDidMount(){
         window.addEventListener('scroll', this.shiftStars);
     }
@@ -12,22 +25,22 @@ class StarField extends React.Component {
     }
 
     shiftStars() {
-        var starField = document.getElementsByClassName('star-field')[0];
-        let top = -Math.ceil((window.scrollY / shiftUpEnd) * 50);
-        starField.style.top = top + "px";
+        this.setState({
+            top: -Math.ceil((window.scrollY / shiftUpEnd) * 50)
+        });
     }
 
     renderStars() {
         var stars = [];
-        for (let i=0; i < 75; i++){
-            stars.push(<Star key={i} />);
+        for (let i=0; i < 100; i++){
+            stars.push(<Star key={i} id={i} />);
         }
         return stars;
     }
 
     render() {
         return (
-            <div className="star-field">
+            <div className="star-field" style={{top: this.state.top}}>
                 {this.renderStars()}
             </div>
         );
@@ -35,20 +48,56 @@ class StarField extends React.Component {
 }
 
 class Star extends React.Component {
+    constructor(props){
+        super(props);
+        let top = this.generateY();
+        let left = this.generateX(); 
+        this.state = {
+            isMounted: true,
+            opacity: Math.random(),
+            fadeDirection: (Math.random() >= 0.5 ? -1 : 1), //Sets the inital direction of the fade randomly
+            top: top,
+            left: left
+        };
+        this.opacityDifference = 0;
+        this.defaultOpacity = this.state.opacity;
+        this.twinkleExecutionDelay = Math.floor(Math.random() * (500 - 100) + 100);
+        this.twinkle = this.twinkle.bind(this);
+    }
 
-    /*componentDidMount(){
-        this.interval = Math.floor(Math.random() * 10000); //Sets interval as some fraction of 10 seconds
-        this.timerID = setInterval(this.twinkle(), this.interval);
-        return;
+    componentDidMount(){
+        var star = this;
+        setTimeout(function(){
+            window.requestAnimationFrame(star.twinkle);
+        }, star.twinkleExecutionDelay);
+        
     }
 
     componentWillUnmount(){
-        clearInterval(this.timerID);
+        this.setState({
+            isMounted: false
+        })
     }
 
     twinkle() {
-        console.log("Hello");
-    }*/
+        var star = this;
+        setTimeout(function(){
+            if (star.state.isMounted) {
+                star.setState({
+                    opacity: star.state.opacity + star.state.fadeDirection/fadeAnimationFrames
+                });
+                star.opacityDifference = Math.abs(star.state.opacity - star.defaultOpacity);
+                //Reverses the direction of the opacity change if opacity is greater than 1, less than zero, 
+                //or if the difference from the default opacity is greater than 0.3
+                if (star.state.opacity > 1 || star.state.opacity < 0 || star.opacityDifference > 0.3){
+                    star.setState({
+                        fadeDirection:  star.state.fadeDirection * -1
+                    });
+                }
+                window.requestAnimationFrame(star.twinkle);
+            }
+        }, fadeAnimationLength/fadeAnimationFrames);
+    }
 
     generateX() {
         return Math.floor(Math.random() * window.innerWidth);
@@ -59,13 +108,8 @@ class Star extends React.Component {
     }
 
     render() {
-        var divStyle = {
-            top: this.generateY(),
-            left: this.generateX()
-        };
-
         return(
-            <div style={divStyle} className="star"></div>
+            <div style={{top: this.state.top, left: this.state.left, opacity: this.state.opacity}} className="star"></div>
         );
     }
 }
